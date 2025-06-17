2423bdb1ed2ca3e4451d1e2e9a7fd8e35fe87398
Jhonz777 <desurenovatio@gmail.com>
2025-06-17 12:44:56 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+alert('Olá, boas vindas ao jogo do número secreto!');
+let numeroSecreto = 5;
+let chute = prompt ('Escola um número entre 1 e 10');
+
+if (chute == numeroSecreto) {
+    console.log('Você acertou! Número secreto é (5)');
+}
+
