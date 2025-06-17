d795e4311400d802d3eca1d856bc14875f6cfb21
Jhonz777 <desurenovatio@gmail.com>
2025-06-17 12:56:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ alert('Olá, boas vindas ao jogo do número secreto!');
 let numeroSecreto = 5;
 let chute = prompt ('Escola um número entre 1 e 10');
 
+// se o chute for igual ao número secreto, exiba uma mensagem de sucesso
 if (chute == numeroSecreto) {
     console.log('Você acertou! Número secreto é (5)');
 }
